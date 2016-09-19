a74787203a128baefa83d712e1369bd4bebe03d4
Vladimir Gladkov <vovkos@gmail.com>
2016-09-19 17:28:18 +0800

    [doxyrest] highlight portions of code between refs; use g_language global variable for specifying language for code-blocks (default to cpp in c_xxx.rst.in frames); propagate 'class' option; add 'overview-code-block' and 'title-code-block' classes to code-blocks in frames

--- sphinx/doxyrest.py
+++ sphinx/doxyrest.py
@@ -1,8 +1,43 @@
 import re
-
 from docutils.parsers.rst import Directive, directives
 from docutils import nodes
 from sphinx import addnodes
+
+
+class HighlightedText(nodes.General, nodes.TextElement):
+    def __init__(self, *args, **kwargs):
+        nodes.TextElement.__init__(self, *args, **kwargs)
+
+
+def visit_highlighted_text_node(self, node):
+    text_node = node.children[0]
+    language = node['language']
+
+    if language == 'none':
+        self.body.append(text_node)
+    else:
+        options = {}
+        options['stripnl'] = False
+        options['ensurenl'] = False
+
+        text = text_node
+
+        # pygments always ensures a newline at the end and there is no way to tell
+        # if the newline is originating from source or was it inserted by pygments
+        # as a workaround, duplicate trailing newline and then strip it
+
+        if text.endswith('\n'):
+            text = text + '\n'
+
+        highlighted = self.highlighter.highlight_block(text, language, options)
+
+        match = re.search('<pre>(.*)\n</pre>', highlighted, re.DOTALL)
+        if not match:
+            self.body.append(text_node)
+        else:
+            self.body.append(match.group(1))
+
+    raise nodes.SkipNode
 
 
 class RefCodeBlock(Directive):
@@ -10,7 +45,16 @@ class RefCodeBlock(Directive):
     required_arguments = 0
     optional_arguments = 1
     final_argument_whitespace = False
-    option_spec = {}
+
+    option_spec = {
+        'linenos': directives.flag,
+        'dedent': int,
+        'lineno-start': int,
+        'emphasize-lines': directives.unchanged_required,
+        'caption': directives.unchanged_required,
+        'class': directives.class_option,
+        'name': directives.unchanged,
+    }
 
     def __init__(self, *args, **kwargs):
         Directive.__init__(self, *args, **kwargs)
@@ -19,17 +63,22 @@ class RefCodeBlock(Directive):
     def run(self):
         code = u'\n'.join(self.content)
         pos = 0
-        node = nodes.literal_block('without-this-no-links-in-the-output', '')
+        node = nodes.literal_block('.', '') # we need non-empty raw_text
+        node['classes'] += ['highlight']    # we are stripping pyments-generated <div>
+        node['classes'] += self.options.get('class', [])
 
         while True:
             match = self.re_prog.search(code, pos)
             if match is None:
                 plain_text = code[pos:]
-                node += nodes.Text(plain_text, plain_text)
+                if plain_text != "":
+                    node += HighlightedText(plain_text, plain_text, language=self.arguments[0])
                 break
 
             plain_text = code[pos:match.start()]
-            node += nodes.Text(plain_text, plain_text)
+            if plain_text != "":
+                node += HighlightedText(plain_text, plain_text, language=self.arguments[0])
+
             node += create_xref_node(match.group(0), match.group(1), match.group(2))
             pos = match.end()
 
@@ -52,4 +101,10 @@ def create_xref_node(raw_text, text, target):
 
 
 def setup(app):
+    app.add_node(
+        HighlightedText,
+        html=(visit_highlighted_text_node, None),
+        latex=(visit_highlighted_text_node, None)
+        )
+
     directives.register_directive('ref-code-block', RefCodeBlock)

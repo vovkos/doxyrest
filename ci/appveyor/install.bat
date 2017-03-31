e5e83c0c6cadb58ba86060a17b37694ddb805564
Vladimir Gladkov <vovkos@gmail.com>
2017-03-31 15:56:21 +0800

    [appveyor] suppress xamarin warning messages on x86 builds

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -55,3 +55,4 @@ echo include (md-to-mt.cmake) >> %DOWNLOAD_DIR%\expat\CMakeLists.txt
 :: Get rid of Xamarin annoying build warnings
 
 del "c:\Program Files (x86)\MSBuild\14.0\Microsoft.Common.targets\ImportAfter\Xamarin.Common.targets"
+del "c:\Program Files (x86)\MSBuild\4.0\Microsoft.Common.targets\ImportAfter\Xamarin.Common.targets"

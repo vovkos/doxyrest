b1db880c9b89b159d8428effc3c6325419cc22c6
Vladimir Gladkov <vovkos@gmail.com>
2017-04-11 17:16:51 +0800

    [travis] build doxyrest even for BUILD_DOC (it builds fast anyway, why complicate scripts)
    also, change make a ahallow clone of axl

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -11,34 +11,23 @@
 
 THIS_DIR=`pwd`
 
+mkdir axl/build
+cd axl/build
+cmake .. -DTARGET_CPU=$TARGET_CPU -DCMAKE_BUILD_TYPE=$BUILD_CONFIGURATION
+make
+
+cd $THIS_DIR
+echo "set (AXL_CMAKE_DIR $THIS_DIR/axl/cmake $THIS_DIR/axl/build/cmake)" >> paths.cmake
+
+mkdir build
+cd build
+cmake .. -DTARGET_CPU=$TARGET_CPU -DCMAKE_BUILD_TYPE=$BUILD_CONFIGURATION
+make
+
 if [ "$BUILD_DOC" != "" ]; then
-	mkdir axl/build
-	cd axl/build
-	cmake .. -DTARGET_CPU=$TARGET_CPU -DCMAKE_BUILD_TYPE=$BUILD_CONFIGURATION
-
-	cd $THIS_DIR
-	echo "set (AXL_CMAKE_DIR $THIS_DIR/axl/cmake $THIS_DIR/axl/build/cmake)" >> paths.cmake
-
-	mkdir build
-	cd build
-	cmake ..
-
 	source doc/index/build-html.sh
 	source doc/manual/build-html.sh
 	source doc/build-guide/build-html.sh
 
 	touch doc/html/.nojekyll
-else
-	mkdir axl/build
-	cd axl/build
-	cmake .. -DTARGET_CPU=$TARGET_CPU -DCMAKE_BUILD_TYPE=$BUILD_CONFIGURATION
-	make
-
-	cd $THIS_DIR
-	echo "set (AXL_CMAKE_DIR $THIS_DIR/axl/cmake $THIS_DIR/axl/build/cmake)" >> paths.cmake
-
-	mkdir build
-	cd build
-	cmake .. -DTARGET_CPU=$TARGET_CPU -DCMAKE_BUILD_TYPE=$BUILD_CONFIGURATION
-	make
 fi

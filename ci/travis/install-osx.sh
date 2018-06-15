64f9d42363c5dcc6d682e31c2d43308cde7ca7c0
Vladimir Gladkov <vladimir@Vladimirs-Mac-mini.local>
2018-06-15 11:44:46 +0800

    [ci] link to lua statically on osx as to avoid lua-5.2 vs lua-5.3 versioning problems

--- ci/travis/install-osx.sh
+++ ci/travis/install-osx.sh
@@ -10,9 +10,18 @@
 #...............................................................................
 
 brew update
-brew install lua
 brew install expat
 brew install ragel
+
+wget $LUA_URL -O $LUA_TAR
+mkdir -p $LUA_DIR
+tar -xzf $LUA_TAR -C $LUA_DIR --exclude=*.dylib
+
+ls -R $LUA_DIR
+
+echo "set (LUA_INC_DIR $LUA_DIR/include)" >> paths.cmake
+echo "set (LUA_LIB_DIR $LUA_DIR)" >> paths.cmake
+echo "set (LUA_LIB_NAME $LUA_LIB_NAME)" >> paths.cmake
 
 if [ "$BUILD_PACKAGE" != "" ]; then
 	brew install fakeroot
--- ci/travis/set-env.sh
+++ ci/travis/set-env.sh
@@ -31,6 +31,18 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	   [ "$BUILD_CONFIGURATION" == "Release" ]; then
 		export BUILD_DOC=ON
 	fi
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
+
+	# it's not as easy to install lua-5.2 side-by-side with lua-5.3 on OSX
+	# we better use static linking to lua as to avoid versioning issues
+
+	LUA_VERSION=5.3.4
+	LUA_LIB_NAME=lua53
+	LUA_OS=MacOS1011
+
+	export LUA_TAR=lua-${LUA_VERSION}_${LUA_OS}_lib.tar.gz
+	export LUA_URL=https://sourceforge.net/projects/luabinaries/files/$LUA_VERSION/Other%20Libraries/${LUA_TAR}/download
+	export LUA_DIR=$TRAVIS_BUILD_DIR/lua-$LUA_VERSION
 fi
 
 if [ "$TRAVIS_TAG" != "" ] && [ "$BUILD_CONFIGURATION" == "Release" ] && \

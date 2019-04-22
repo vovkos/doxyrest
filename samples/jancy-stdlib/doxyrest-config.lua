fb963105d4a19fc9026de4f1491c385a07faf389
Vladimir Gladkov (laptop-arch) <vovkos@gmail.com>
2019-04-22 10:30:33 +0800

    [samples] moved definitions into doxyrest-config.lua files

---
+++ samples/jancy-stdlib/doxyrest-config.lua
@@ -0,0 +1,12 @@
+LANGUAGE = "jancy"
+ML_PARAM_LIST_COUNT_THRESHOLD = 1
+INTRO_FILE = "intro.rst"
+LANGUAGE = "jancy"
+FORCE_INCLUDE_FILE = "def.rst"
+
+EXTRA_PAGE_LIST =
+{
+	"m_syncId.rst",
+	"disposable.rst",
+	"errorcode.rst"
+}

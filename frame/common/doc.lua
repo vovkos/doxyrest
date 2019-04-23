ae8ae5118fd6e5d3e9826e3d9fb10a45ebc2a567
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2019-04-23 17:43:10 +0800

    [frame] re-factored getDocBlockContents (split into multiple block-kind-specific functions looked up via a map)

--- frame/common/doc.lua
+++ frame/common/doc.lua
@@ -29,13 +29,105 @@ function concatDocBlockContents(s1, s2)
 	end
 end
 
-function processListDocBlock(block, context, bullet)
-	local s
+function getCodeDocBlockContents(block, context)
+	context.codeBlockKind = block.blockKind
+	local s = getDocBlockListContentsImpl(block.childBlockList, context)
+	context.codeBlockKind = nil
 
+	return s
+end
+
+function getDocBlockText(block, context)
+	local text = block.text
+	local childContents = getDocBlockListContentsImpl(block.childBlockList, context)
+
+	if not context.codeBlockKind then
+		if ESCAPE_ASTERISKS then
+			text = string.gsub(text, "%*", "\\*")
+		end
+
+		text = trimWhitespace(text)
+		text = concatDocBlockContents(text, childContents)
+	else
+		text = text .. childContents
+	end
+
+	return text
+end
+
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+-- handling of different block kinds
+
+function formatDocBlock_computeroutput(block, context)
+	if context.codeBlockKind then
+		return block.text .. getDocBlockListContentsImpl(block.childBlockList, context)
+	end
+
+	local code = block.text .. getCodeDocBlockContents(block, context)
+	if not string.find(code, "\n") then
+		return "``" .. trimWhitespace(code) .. "``"
+	end
+
+	code = replaceCommonSpacePrefix(code, "\t")
+	code = trimTrailingWhitespace(code)
+	return "\n\n.. code-block:: none\n\n" .. code
+end
+
+function formatDocBlock_programlisting(block, context)
+	local code = getCodeDocBlockContents(block, context)
+	code = replaceCommonSpacePrefix(code, "\t")
+	code = trimTrailingWhitespace(code)
+
+	return "\n\n.. ref-code-block:: " .. LANGUAGE .. "\n\n" .. code .. "\n\n"
+end
+
+function formatDocBlock_preformatted(block, context)
+	local code = getCodeDocBlockContents(block, context)
+	code = replaceCommonSpacePrefix(code, "\t")
+	code = trimTrailingWhitespace(code)
+
+	-- raw seems like a better approach, but need to figure something out with indents
+	return "\n\n.. code-block:: none\n\n" .. code .. "\n\n"
+end
+
+function formatDocBlock_formula(block, context)
+	local code = getCodeDocBlockContents(block, context)
+	local isInline = string.sub(code, 1, 1) == "$"
+
+	-- take away framing tokens
+	code = string.gsub(code, "^\\?[$%[]", "")
+	code = string.gsub(code, "\\?[$%]]$", "")
+
+	if isInline then
+		return ":math:`" .. code .. "`"
+	end
+
+	code = replaceCommonSpacePrefix(code, "\t")
+	code = trimTrailingWhitespace(code)
+
+	-- raw seems like a better approach, but need to figure something out with indents
+	return "\n\n.. math::\n\n" .. code .. "\n\n"
+end
+
+function formatDocBlock_verbatim(block, context)
+	if not VERBATIM_TO_CODE_BLOCK then
+		return getDocBlockText(block, context)
+	end
+
+	local code = getCodeDocBlockContents(block, context)
+	code = replaceCommonSpacePrefix(code, "\t")
+	code = trimTrailingWhitespace(code)
+
+	-- probably also need to assign some div class
+	return "\n\n.. code-block:: " .. VERBATIM_TO_CODE_BLOCK .. "\n\n" .. code .. "\n\n"
+end
+
+function formatDocBlock_list(block, context, bullet)
 	local prevBullet = context.listItemBullet
 	context.listItemBullet = bullet
 
-	s = getDocBlockListContentsImpl(block.childBlockList, context)
+	local s = getDocBlockListContentsImpl(block.childBlockList, context)
 	s = "\n\n" .. trimTrailingWhitespace(s) .. "\n\n"
 
 	context.listItemBullet = prevBullet
@@ -43,7 +135,7 @@ function processListDocBlock(block, context, bullet)
 	return s
 end
 
-function processDlListDocBlock(block, context)
+function formatDocBlock_variablelist(block, context)
 	local prevList = context.dlList
 	context.dlList = {}
 
@@ -69,229 +161,283 @@ function processDlListDocBlock(block, context)
 	return s
 end
 
-function getCodeDocBlockContents(block, context)
-	context.codeBlockKind = block.blockKind
-	local s = getDocBlockListContentsImpl(block.childBlockList, context)
-	context.codeBlockKind = nil
+function formatDocBlock_ref(block, context)
+	local text = getDocBlockText(block, context)
+	string.gsub(text, "<", "\\<") -- escape left chevron
+	return ":ref:`" .. text .. " <doxid-" .. block.id .. ">`"
+end
+
+function formatDocBlock_anchor(block, context)
+	local text = getDocBlockText(block, context)
+	return ":target:`doxid-" .. block.id .. "`" .. text
+end
+
+function formatDocBlock_image(block, context)
+	local s = "\n\n.. image:: " .. block.name .. "\n"
+	if block.width ~= 0 then
+		s = s .. "\t:width: " .. block.width .. "\n"
+	end
+	if block.height ~= 0 then
+		s = s .. "\t:height: " .. block.height .. "\n"
+	end
+	if block.text ~= "" then
+		s = s .. "\t:alt: " .. block.text .. "\n"
+	end
+
+	return s .. "\n"
+end
+
+function formatDocBlock_font(block, context, token)
+	local text = getDocBlockText(block, context)
+	if not string.find(text, "\n") then -- single line only
+		text = token .. text .. token
+	end
+
+	return text
+end
+
+function formatDocBlock_heading(block, context)
+	local text = getDocBlockText(block, context)
+	return ".. rubric:: " .. text .. "\n\n"
+end
+
+function formatDocBlock_heading(block, context)
+	local text = getDocBlockText(block, context)
+	return ".. rubric:: " .. text .. "\n\n"
+end
+
+function formatDocBlock_varlistentry(block, context)
+	if not context.dlList then
+		error("unexpected <varlistentry>")
+	end
+
+	local text = getDocBlockText(block, context)
+	local count = #context.dlList
+	local entry = {}
+	entry.title = trimWhitespace(text)
+	entry.description = ""
+	context.dlList[count + 1] = entry
+
+	return ""
+end
+
+function formatDocBlock_listitem(block, context)
+	local s = ""
+	local text = getDocBlockText(block, context)
+
+	if context.dlList then
+		local count = #context.dlList
+		local entry = context.dlList[count]
+		if entry then
+			entry.description = trimWhitespace(text)
+		end
+	else
+		if not context.listItemBullet then
+			error("unexpected <listitem>")
+		end
+
+		s = context.listItemBullet .. " "
+		local indent = string.rep(' ', string.len(s))
+
+		text = replaceCommonSpacePrefix(text, indent)
+		text = trimWhitespace(text)
+
+		s = s .. text .. "\n\n"
+	end
 
 	return s
 end
 
-function getDocBlockContents(block, context)
+function formatDocBlock_para(block, context)
+	local s = getDocBlockText(block, context)
+	s = trimWhitespace(s)
+
+	if s ~= "" then
+		s = s .. "\n\n"
+	end
+
+	return s
+end
+
+function formatDocBlock_parametername(block, context)
+	local text = getDocBlockText(block, context)
+	text = trimWhitespace(text)
+
+	if not context.paramSection then
+		context.paramSection = {}
+	end
+
+	local i = #context.paramSection + 1
+
+	context.paramSection[i] = {}
+	context.paramSection[i].name = text
+	context.paramSection[i].description = ""
+
+	return ""
+end
+
+function formatDocBlock_parameterdescription(block, context)
+	local text = getDocBlockText(block, context)
+	text = trimWhitespace(text)
+
+	if string.find(text, "\n") then
+		text = "\n" .. replaceCommonSpacePrefix(text, "\t\t  ") -- add paramter table offset "- "
+	end
+
+	if context.paramSection then
+		local count = #context.paramSection
+		context.paramSection[count].description = text
+	end
+
+	return ""
+end
+
+function formatDocBlock_sect(block, context)
 	local s = ""
+	local text = getDocBlockText(block, context)
 
-	if block.blockKind == "computeroutput" then
-		if context.codeBlockKind then
-			s = block.text .. getDocBlockListContentsImpl(block.childBlockList, context)
-		else
-			local code = block.text .. getCodeDocBlockContents(block, context)
+	if block.id and block.id ~= "" then
+		s = ".. _doxid-" .. block.id .. ":\n"
+	end
 
-			if not string.find(code, "\n") then
-				s = "``" .. trimWhitespace(code) .. "``"
-			else
-				code = replaceCommonSpacePrefix(code, "\t")
-				code = trimTrailingWhitespace(code)
-				s = "\n\n.. code-block:: none\n\n" .. code
-			end
-		end
-	elseif block.blockKind == "programlisting" then
-		local code = getCodeDocBlockContents(block, context)
-		code = replaceCommonSpacePrefix(code, "\t")
-		code = trimTrailingWhitespace(code)
+	if block.title and block.title ~= "" then
+		s = s .. ".. rubric:: " .. block.title .. ":\n"
+	end
 
-		s = "\n\n.. ref-code-block:: " .. LANGUAGE .. "\n\n" .. code .. "\n\n"
-	elseif block.blockKind == "preformatted" then
-		local code = getCodeDocBlockContents(block, context)
-		code = replaceCommonSpacePrefix(code, "\t")
-		code = trimTrailingWhitespace(code)
-
-		-- raw seems like a better approach, but need to figure something out with indents
-		s = "\n\n.. code-block:: none\n\n" .. code .. "\n\n"
-	elseif block.blockKind == "formula" then
-		local code = getCodeDocBlockContents(block, context)
-		local isInline = string.sub(code, 1, 1) == "$"
-
-		-- take away framing tokens
-		code = string.gsub(code, "^\\?[$%[]", "")
-		code = string.gsub(code, "\\?[$%]]$", "")
-
-		if isInline then
-			s = ":math:`" .. code .. "`"
-		else
-			code = replaceCommonSpacePrefix(code, "\t")
-			code = trimTrailingWhitespace(code)
-
-			-- raw seems like a better approach, but need to figure something out with indents
-			s = "\n\n.. math::\n\n" .. code .. "\n\n"
-		end
-	elseif block.blockKind == "verbatim" and VERBATIM_TO_CODE_BLOCK then
-		local code = getCodeDocBlockContents(block, context)
-		code = replaceCommonSpacePrefix(code, "\t")
-		code = trimTrailingWhitespace(code)
-
-		-- probably also need to assign some div class
-		s = "\n\n.. code-block:: " .. VERBATIM_TO_CODE_BLOCK .. "\n\n" .. code .. "\n\n"
-	elseif block.blockKind == "itemizedlist" then
-		s = processListDocBlock(block, context, "*")
-	elseif block.blockKind == "orderedlist" then
-		s = processListDocBlock(block, context, "#.")
-	elseif block.blockKind == "variablelist" then
-		s = processDlListDocBlock(block, context)
+	if s ~= "" then
+		s = s .. "\n" .. text
 	else
-		local text = block.text
-		local childContents = getDocBlockListContentsImpl(block.childBlockList, context)
+		s = text
+	end
 
-		if not context.codeBlockKind then
-			if ESCAPE_ASTERISKS then
-				text = string.gsub(text, "%*", "\\*")
-			end
+	return s
+end
 
-			text = trimWhitespace(text)
-			text = concatDocBlockContents(text, childContents)
-		else
-			text = text .. childContents
+function formatDocBlock_simplesect(block, context)
+	local s = ""
+	local text = getDocBlockText(block, context)
 
-			if context.codeBlockKind ~= "programlisting" then
-				return text
-			end
+	if block.simpleSectionKind == "return" then
+		if not context.returnSection then
+			context.returnSection = {}
 		end
 
-		if block.blockKind == "linebreak" then
-			s = "\n\n"
-		elseif block.blockKind == "ref" then
-			text = string.gsub(text, "<", "\\<") -- escape left chevron
-			s = ":ref:`" .. text .. " <doxid-" .. block.id .. ">`"
-		elseif block.blockKind == "anchor" then
-			s = ":target:`doxid-" .. block.id .. "`" .. text
-		elseif block.blockKind == "image" then
-			s = "\n\n.. image:: " .. block.name .. "\n"
-			if block.width ~= 0 then
-				s = s .. "\t:width: " .. block.width .. "\n"
-			end
-			if block.height ~= 0 then
-				s = s .. "\t:height: " .. block.height .. "\n"
-			end
-			if block.text ~= "" then
-				s = s .. "\t:alt: " .. block.text .. "\n"
-			end
-			s = s .. "\n"
-		elseif block.blockKind == "bold" then
-			if not string.find(text, "\n") then -- single line only
-				s = "**" .. text .. "**"
-			else
-				s = text
-			end
-		elseif block.blockKind == "emphasis" then
-			if not string.find(text, "\n") then -- single line only
-				s = "*" .. text .. "*"
-			else
-				s = text
-			end
-		elseif block.blockKind == "heading" then
-			s = ".. rubric:: " .. text .. "\n\n"
-		elseif block.blockKind == "sp" then
-			s = " "
-		elseif block.blockKind == "varlistentry" then
-			if not context.dlList then
-				error("unexpected <varlistentry>")
-			end
+		local count = #context.returnSection
+		context.returnSection[count + 1] = text
+	elseif block.simpleSectionKind == "see" then
+		if not context.seeSection then
+			context.seeSection = {}
+		end
 
-			local count = #context.dlList
-			local entry = {}
-			entry.title = trimWhitespace(text)
-			entry.description = ""
-			context.dlList[count + 1] = entry
-		elseif block.blockKind == "listitem" then
-			if context.dlList then
-				local count = #context.dlList
-				local entry = context.dlList[count]
-				if entry then
-					entry.description = trimWhitespace(text)
+		local count = #context.seeSection
+		context.seeSection[count + 1] = text
+	else
+		s = text
+	end
+
+	return s
+end
+
+function formatDocBlock_ulink(block, context)
+	return "`" .. block.text .. " <" .. block.url .. ">`_"
+end
+
+function formatDocBlock_table(b, context)
+	local blockList = b.childBlockList
+	local tbl = {}
+	local maxwidths = {}
+
+	for i = 1, #blockList do
+		local block = blockList [i]
+		if block.blockKind == 'row' then
+			local rowblock = block.childBlockList
+			local row = {}
+			for rownum = 1, #rowblock do
+				if rowblock[rownum].blockKind == 'entry' then
+					local entryblock = rowblock[rownum].childBlockList
+					for entry = 1, #entryblock do
+						if entryblock[entry].blockKind == 'para' then
+							local t = trimWhitespace(entryblock[entry].childBlockList[1].text)
+							local tlen = string.len(t)
+							table.insert(row, t)
+							if maxwidths[#row] == nil or maxwidths[#row] < tlen then
+								maxwidths[#row] = tlen
+							end
+						end
+					end
 				end
-			else
-				if not context.listItemBullet then
-					error("unexpected <listitem>")
-				end
-
-				s = context.listItemBullet .. " "
-				local indent = string.rep(' ', string.len(s))
-
-				text = replaceCommonSpacePrefix(text, indent)
-				text = trimWhitespace(text)
-
-				s = s .. text .. "\n\n"
 			end
-		elseif block.blockKind == "para" then
-			s = trimWhitespace(text)
-			if s ~= "" then
-				s = s .. "\n\n"
-			end
-		elseif block.blockKind == "parametername" then
-			text = trimWhitespace(text)
+			table.insert(tbl, row)
+		end
+	end
 
-			if not context.paramSection then
-				context.paramSection = {}
-			end
+	if #tbl == 0 then
+		return ''
+	end
 
-			local i = #context.paramSection + 1
+	local headfoot = ''
+	for i = 1, #maxwidths do
+		headfoot = headfoot .. string.rep('=', maxwidths[i]) .. '  '
+	end
 
-			context.paramSection[i] = {}
-			context.paramSection[i].name = text
-			context.paramSection[i].description = ""
-		elseif block.blockKind == "parameterdescription" then
-			text = trimWhitespace(text)
+	local s = headfoot .. '\n'
 
-			if string.find(text, "\n") then
-				text = "\n" .. replaceCommonSpacePrefix(text, "\t\t  ") -- add paramter table offset "- "
-			end
-
-			if context.paramSection then
-				local count = #context.paramSection
-				context.paramSection[count].description = text
-			end
-		elseif string.match(block.blockKind, "sect[1-4]") then
-			if block.id and block.id ~= "" then
-				s = ".. _doxid-" .. block.id .. ":\n"
-			end
-
-			if block.title and block.title ~= "" then
-				s = s .. ".. rubric:: " .. block.title .. ":\n"
-			end
-
-			if s ~= "" then
-				s = s .. "\n" .. text
-			else
-				s = text
-			end
-		elseif block.blockKind == "simplesect" then
-			if block.simpleSectionKind == "return" then
-				if not context.returnSection then
-					context.returnSection = {}
-				end
-
-				local count = #context.returnSection
-				context.returnSection[count + 1] = text
-			elseif block.simpleSectionKind == "see" then
-				if not context.seeSection then
-					context.seeSection = {}
-				end
-
-				local count = #context.seeSection
-				context.seeSection[count + 1] = text
-			else
-				s = text
-			end
-		elseif block.m_blockKind == "ulink" then
-			s = "`" .. block.m_text .. "<" .. block.m_url .. ">`_"
-		elseif block.m_blockKind == "table" then
-			s = getSimpleTableContents(block.m_childBlockList, context)
-		else
-			s = text
+	for r = 1, #tbl do
+		for c = 1, #tbl[r] do
+			s = s .. tbl[r][c] .. string.rep(' ', 2 + maxwidths[c] - string.len(tbl[r][c]))
+		end
+		s = s .. '\n'
+		if r == 1 or r == #tbl then
+			s = s .. headfoot .. '\n'
 		end
 	end
 
 	return s
 end
+
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+g_blockKindFormatMap =
+{
+	["computeroutput"]       = formatDocBlock_computeroutput,
+	["programlisting"]       = formatDocBlock_programlisting,
+	["preformatted"]         = formatDocBlock_preformatted,
+	["formula"]              = formatDocBlock_formula,
+	["verbatim"]             = formatDocBlock_verbatim,
+	["itemizedlist"]         = function(b, c) return formatDocBlock_list(b, c, "*") end,
+	["orderedlist"]          = function(b, c) return formatDocBlock_list(b, c, "#.") end,
+	["variablelist"]         = formatDocBlock_variablelist,
+	["linebreak"]            = function(b, c) return "\n\n" end,
+	["ref"]                  = formatDocBlock_ref,
+	["anchor"]               = formatDocBlock_anchor,
+	["image"]                = formatDocBlock_image,
+	["bold"]                 = function(b, c) return formatDocBlock_font(b, c, "**") end,
+	["emphasis"]             = function(b, c) return formatDocBlock_font(b, c, "*") end,
+	["sp"]                   = function(b, c) return " " end,
+	["varlistentry"]         = formatDocBlock_varlistentry,
+	["listitem"]             = formatDocBlock_listitem,
+	["para"]                 = formatDocBlock_para,
+	["parametername"]        = formatDocBlock_parametername,
+	["parameterdescription"] = formatDocBlock_parameterdescription,
+	["sect1"]                = formatDocBlock_sect,
+	["sect2"]                = formatDocBlock_sect,
+	["sect3"]                = formatDocBlock_sect,
+	["sect4"]                = formatDocBlock_sect,
+	["simplesect"]           = formatDocBlock_simplesect,
+	["ulink"]                = formatDocBlock_ulink,
+	["table"]                = formatDocBlock_table,
+}
+
+function getDocBlockContents(block, context)
+	format = g_blockKindFormatMap[block.blockKind]
+	if format then
+		return format(block, context)
+	else
+		return getDocBlockText(block, context)
+	end
+end
+
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 function getDocBlockListContentsImpl(blockList, context)
 	local s = ""
@@ -354,58 +500,6 @@ function getSimpleDocBlockListContents(blockList)
 	for i = 1, #blockList do
 		local block = blockList[i]
 		s = s .. block.text .. getSimpleDocBlockListContents(block.childBlockList)
-	end
-
-	return s
-end
-
-function getSimpleTableContents(blockList, context)
-	local tbl = {}
-	local maxwidths = {}
-
-	for i = 1, #blockList do
-		local block = blockList [i]
-		if block.m_blockKind == 'row' then
-			local rowblock = block.m_childBlockList
-			local row = {}
-			for rownum = 1, #rowblock do
-				if rowblock[rownum].m_blockKind == 'entry' then
-					local entryblock = rowblock[rownum].m_childBlockList
-					for entry = 1, #entryblock do
-						if entryblock[entry].m_blockKind == 'para' then
-							local t = trimWhitespace(entryblock[entry].m_childBlockList[1].m_text)
-							local tlen = string.len(t)
-							table.insert(row, t)
-							if maxwidths[#row] == nil or maxwidths[#row] < tlen then
-								maxwidths[#row] = tlen
-							end
-						end
-					end
-				end
-			end
-			table.insert(tbl, row)
-		end
-	end
-
-	if #tbl == 0 then
-		return ''
-	end
-
-	local headfoot = ''
-	for i = 1, #maxwidths do
-		headfoot = headfoot .. string.rep('=', maxwidths[i]) .. '  '
-	end
-
-	s = headfoot .. '\n'
-
-	for r = 1, #tbl do
-		for c = 1, #tbl[r] do
-			s = s .. tbl[r][c] .. string.rep(' ', 2 + maxwidths[c] - string.len(tbl[r][c]))
-		end
-		s = s .. '\n'
-		if r == 1 or r == #tbl then
-			s = s .. headfoot .. '\n'
-		end
 	end
 
 	return s

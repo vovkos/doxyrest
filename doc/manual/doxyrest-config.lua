ba03bd72a55c13692558e5663eab0ffc618d7b2f
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2019-05-06 18:22:26 +0800

    [doc] added doxyrest-config.lua for manual

---
+++ doc/manual/doxyrest-config.lua
@@ -0,0 +1,14 @@
+﻿--------------------------------------------------------------------------------
+--
+--  This file is part of the Doxyrest toolkit.
+--
+--  Doxyrest is distributed under the MIT license.
+--  For details see accompanying license.txt file,
+--  the public copy of which is also available at:
+--  http://tibbo.com/downloads/archive/doxyrest/license.txt
+--
+--------------------------------------------------------------------------------
+
+GLOBAL_AUX_COMPOUND_ID = "group_api_99_global"
+SORT_GROUPS_BY         = "id"
+SORT_GLOBAL_MEMBERS    = true

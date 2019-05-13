0eb9f7ae363ff7ab74e4556a975af9754af161bb
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2019-05-13 20:38:06 +0800

    [doc] added a draft for a page re documenting unnamed structs

--- doc/frame/cfamily/doxyrest-config.lua
+++ doc/frame/cfamily/doxyrest-config.lua
@@ -13,3 +13,4 @@ INTRO_FILE             = "intro.rst"
 GLOBAL_AUX_COMPOUND_ID = "group_global"
 SORT_GROUPS_BY         = "id"
 SORT_GLOBAL_MEMBERS    = true
+EXTRA_PAGE_LIST        = { "unnamed-structs.rst" }
